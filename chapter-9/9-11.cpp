7d5f41349e510a6e01bba88f8b981e25e4fed74d
freddyhm <freddy.hidalgo.m@gmail.com>
2018-02-21 09:32:19 -0800

    add buildHistogram and areHistogramEqual functions

--- chapter-9/9-11.cpp
+++ chapter-9/9-11.cpp
@@ -12,6 +12,7 @@
 
 #include <iostream>
 #include <string>
+#include <cctype>
 
 using namespace std;
 
@@ -20,12 +21,32 @@ using namespace std;
 	postcondition: returns true if both strings contain the same letters */
 bool isAnagram(string firstWord, string secondWord);
 
+/*	precondition: args are an empty array and a string that may contain punctuation, 
+				  spaces, and a mix of upper and lower case
+	postcondition: builds a histogram with an array containing the frequency of characters  */
+void buildHistogram(int letterFreq[], string s);
+
+/*	precondition: two non-empty arrays and sizeHistogram > 0
+	postcondition: returs true if both arrays have the same values and in the same order  */
+bool areHistogramEqual(int letterCount1[], int letterCount2[], int sizeHistogram);
+
 int main()
 {
-	string firstWord = "Eleven plus two";
-	string secondWord = "Twelve plus two";
+	string firstWord;
+	string secondWord;
 
-	cout << isAnagram(firstWord, secondWord) << endl;
+	cout << "Enter two strings" << endl;
+	getline(cin, firstWord);
+	getline(cin, secondWord);
+
+	if (isAnagram(firstWord, secondWord))
+	{
+		cout << firstWord + " and " + secondWord + " are anagramas!" << endl;
+	}
+	else
+	{
+		cout << firstWord + " and " + secondWord + " are not anagramas!" << endl;
+	}
 
 	system("pause");
 
@@ -34,7 +55,44 @@ int main()
 
 bool isAnagram(string firstWord, string secondWord)
 {
+	const int ALPHA_SIZE = 26;
+	int firstHistogram[26] = {};
+	int secondHistogram[26] = {};
+	
+	buildHistogram(firstHistogram, firstWord);
+	buildHistogram(secondHistogram, secondWord);
 
-
-	return true;
+	return areHistogramEqual(firstHistogram, secondHistogram, ALPHA_SIZE);
 }
+
+bool areHistogramEqual(int letterCount1[], int letterCount2[], int sizeHistogram)
+{
+	bool result = true;
+
+	for (int i = 0; i < sizeHistogram; i++)
+	{
+		if (letterCount1[i] != letterCount2[i])
+		{
+			result = false;
+		}
+	}
+
+	return result;
+}
+
+void buildHistogram(int letterCount[], string word)
+{
+	int numLetter = 0;
+
+	// add letter's number equivalent to histogram 
+	for (int i = 0; i < word.length(); i++)
+	{
+		if (isalpha(word[i]))
+		{
+			char letter = tolower(word[i]); 
+			numLetter = static_cast<int>(letter) - 97; // sets a = 0
+			letterCount[numLetter]++;
+		}
+	}
+}
+
